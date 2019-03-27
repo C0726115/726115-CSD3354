67ddae5853fb35651f4014da6b42772b1093dc3d
visualstudio <visualstudio@122-PC26>
2019-03-27 11:54:00 -0400

    Commit 11:50 am

--- 726115-CSD3354/Program.cs
+++ 726115-CSD3354/Program.cs
@@ -1,31 +1,40 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace _726115_CSD3354
+namespace LearningAsyncAwayStart
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Downloading file");
+            Console.WriteLine("downloading file");
             Download();
             Console.ReadLine();
 
         }
 
-
         static void Download()
         {
+            Network.Download((message) => Console.WriteLine("Download complete " + message));
+        }
+    }
+
+    // Imaginary external network library
+
+    class Network
+    {
+        static public void Download(Action<String> callback)
+        {
             Task.Run(() => {
-
                 Thread.Sleep(3000);
-                Console.WriteLine("Download  Complete");
+                callback("completed");
             });
-
         }
     }
 }
